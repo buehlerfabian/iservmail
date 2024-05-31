73441883f145bd5ed604213fd9949c73c0470281
Fabian Bühler <buehler.fabian@gmx.de>
2024-05-31 11:24:47 +0200

    implemented github copilot suggestion as comment

--- iservmail/iserv_smtpserver.py
+++ iservmail/iserv_smtpserver.py
@@ -26,3 +26,42 @@ def run():
 
 if __name__ == '__main__':
     run()
+
+# import asyncio
+# import email
+# from aiosmtpd.handlers import Message
+# from aiosmtpd.controller import Controller
+# from iservconnection import IservConnection
+
+
+# class IservHandler(Message):
+
+#     async def handle_DATA(self, server, session, envelope):
+#         msg = email.message_from_bytes(envelope.content)
+
+#         with IservConnection() as con:
+#             success = con.send_mail(
+#                 receiver=envelope.rcpt_tos,
+#                 subject=msg['Subject'],
+#                 body=msg.get_payload(decode=True).decode()
+#             )
+#             if not success:
+#                 return '550 Mail could not be sent'
+#         return '250 Message accepted for delivery'
+
+
+# def run():
+#     loop = asyncio.get_event_loop()
+#     controller = Controller(IservHandler(), hostname='127.0.0.1', port=1025)
+#     controller.start()
+
+#     try:
+#         loop.run_forever()
+#     except KeyboardInterrupt:
+#         pass
+#     finally:
+#         controller.stop()
+
+
+# if __name__ == '__main__':
+#     run()
